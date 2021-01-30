d343516f57a27f1720b4a612e124e327524294ad
Carmine <Carmineporricelli96@gmail.com>
2021-01-30 12:06:46 +0100

    added metatags

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Background from "./../components/Canvas";
-import { useState } from "react";
-
+import Head from "next/head";
 export default function Index() {
   return (
     <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="description" content="Qui si scopre se carmine (il reale, non Carmine-Bot) ti vuole bene. Se sei qui, molto probabilmente sì." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#fe7f6c" />
+        <title>Carmine ti vuole bene? | carminemivuolebene.site</title>
+      </Head>
       <Background />
     </>
   );
