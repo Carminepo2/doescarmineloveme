b6fc5165930cd6c4c3bb95629381ec40a2470fc3
Carmine <Carmineporricelli96@gmail.com>
2021-01-30 16:32:31 +0100

    added Google font

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#fe7f6c" />
         <title>Carmine ti vuole bene? | carminemivuolebene.site</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap" rel="stylesheet"></link>
       </Head>
       <Background fps={15} msg="Molto" heartsNumber={50} />
     </>
